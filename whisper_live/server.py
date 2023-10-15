ad299c2dd11ad11c53097a634813cf0bc0cb11ec
theo77186 <theo77186@gmail.com>
2023-10-15 13:50:03 +0200

    Use large-v2 model

--- whisper_live/server.py
+++ whisper_live/server.py
@@ -215,7 +215,7 @@ class ServeClient:
         self.task = task
         device = "cuda" if torch.cuda.is_available() else "cpu"
         self.transcriber = WhisperModel(
-            "small" if multilingual else "small.en", 
+            "large-v2" if multilingual else "large-v2",
             device=device,
             compute_type="int8" if device=="cpu" else "float16", 
             local_files_only=False,
